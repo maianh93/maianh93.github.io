a454e88cfb0a6121db7c7d6fa6453a592455d92c
maianh93 <93634891+maianh93@users.noreply.github.com>
2021-11-24 14:53:26 +0700

    lesson_11

--- lesson_11/homework_lesson_11/main.js
+++ lesson_11/homework_lesson_11/main.js
@@ -60,7 +60,7 @@ function testPrimeNumber(number) {
     if (number == 2) {
         return true
     }
-    for (let i = 3; i < number; i++) {
+    for (let i = 2; i < number; i++) {
         if (number % i == 0) {
             return false;
         }
