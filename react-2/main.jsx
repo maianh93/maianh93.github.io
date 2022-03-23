910d19d72d9676bd861989ef5b1c9ed7192b02ee
maianh93 <93634891+maianh93@users.noreply.github.com>
2022-03-23 16:13:35 +0700

    react-2

--- react-2/main.jsx
+++ react-2/main.jsx
@@ -9,11 +9,22 @@ const InputField = (props) => {
             value={props.value}
             onChange={props.onChange} 
             placeholder={props.placeholder} />
+            <span>{props.unit}</span>
         </div>
-
+        
     </div>
 };
 
+const Comment = (props) => {
+    return (
+      <div>
+        {props.values < 18.5 && props.values > 0 && <b>You are underweight!!!</b>}
+        {props.values < 22.9 && props.values >= 18.5 && <b>You have a good body!!!</b>}
+        {props.values >= 23 && props.values < 25 && <b>You are considered overweight!!!</b>}
+        {props.values >= 25 && <b>You are considered obese!!!!</b>}
+      </div>
+    );
+  };
 
 
 InputField.propTypes = {
@@ -27,39 +38,11 @@ InputField.propTypes = {
 
 const App = () => {
     //Hooks
-    const [bmi, setBmi] = useState(0); //state, setState
+    const [bmi, setBmi] = useState(null); //state, setState
     const [values, setValues] = useState({
         weight: null,
         height: null,
     })
-
-    const result = [
-        {
-            bmi: 1,
-            text: "Please fill infomation",
-            message: ""
-        },
-        {
-            bmi: 18.5,
-            text: "You are underweight!!!",
-            message: "You need to apply diet and exercise to gain weight."
-        },
-        {   
-            bmi: 25,
-            text: "You have a good body!!!",
-            message: "You should maintain a healthy diet and exercise."
-        },
-        {
-            bmi: 30,
-            text: "You are considered overweight!!!",
-            message: "The situation is not too serious, so you can immediately find effective natural weight loss methods and combine exercise. If applied in moderation and persistently, it only takes a few months for you to have a very satisfactory body."
-        },
-        {
-            bmi: Infinity,
-            text: "You are considered obese!!!",
-            message: "You can face a lot of pressure on a daily basis, especially the weight of fat puts pressure on bones and organs, affecting your performance and health."
-        }
-    ]
 
     const handleChange = (e) => {
         setValues({
@@ -74,15 +57,17 @@ const App = () => {
         e.preventDefault(); //important
         //Uncontrolled form
 
-        const { weight, height } = e.target.elements;
+        if (weight == "" || height == "") {
+            alert("Please fill infomation!");
+            return;
+          }
 
-        const bmi = (weight.value / ((height.value)/100 * (height.value)/100)).toFixed(2)
+        const { weight, height } = values;
 
-        setBmi(bmi)
+        const resultBmi = (weight / ((height) * (height))).toFixed(2)
+
+        setBmi(resultBmi)
     };
-
-    const resultText = result.find(e => bmi <= e.bmi).text;
-    const messageText = result.find(e => bmi <= e.bmi).message;
 
 
     return (
@@ -91,32 +76,27 @@ const App = () => {
 
             <form onSubmit={handleSubmit} action="">
                 <InputField
-                    label="Weight"
                     id="weight"
                     onChange={handleChange}
-                    value={values.weight}
+                    value={values.weight || ""}
                     type="number" 
                     name="weight"
-                    placeholder="kg"
+                    placeholder="Weight"
+                    unit="kg"
                 />
                 <InputField
-                    label="Height"
                     id="height"
                     onChange={handleChange}
-                    value={values.height}
+                    value={values.height || ""}
                     type="number"
-                    placeholder="cm"
+                    placeholder="Height"
                     name="height"
+                    unit="m"
                 />
                 <p className="result">
                     Your BMI: <b>{bmi}</b>
                 </p>
-                <p className="result-text">
-                    {resultText}
-                </p>
-                <p className="result-message">
-                    {messageText}
-                </p>
+                <Comment values={bmi} />
             <button className="btn-calculate">Calculate</button>
             </form>
 
