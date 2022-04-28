a195c09f7a4a5ffe51d1ecf0e08b5555518369b6
maianh <maianhphung291093@gmail.com>
2022-04-28 10:38:30 +0700

    header footer

---
+++ react-project/src/App.jsx
@@ -0,0 +1,21 @@
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useContext,
+  useMemo,
+  useRef,
+  useReducer,
+} from "react";
+import "./App.css";
+
+const AppContext = React.createContext({});
+
+// AppContext.Provider value = {{}}
+// AppContext.Consumer => useContext(AppContext) value
+
+function App() {
+
+}
+
+export default App;
---
+++ react-project/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css"
+
+const Home = () => {
+    return (
+      <section className={styles.home}>
+        <h2>Home Page</h2>
+      </section>
+    );
+  };
+  
+  export default Home;
